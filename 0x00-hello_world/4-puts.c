c370a98c2ef9ac7a45af4b76ea564c7928878e1c
Akamo Philip <pakamo20@gmail.com>
2023-05-22 11:04:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 	/**
-	*main entry point of code
-	*puts function to print
-	*return success
-	*/
+	 *main - Entry point
+	 *puts function to print
+	 *return: success
+	 */
 	int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
