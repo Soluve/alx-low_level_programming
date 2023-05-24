1b89f70bbb7ea64ff37ef8baa4a05c3c90ef4818
Akamo Philip <pakamo20@gmail.com>
2023-05-24 03:13:10 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of code
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar('a' + 1);
+	}
+
+	return (0);
+}
