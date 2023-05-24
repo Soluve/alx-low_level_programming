9a1f63f6ce0dcdad8ba4586bf70f0033c41001e3
Akamo Philip <pakamo20@gmail.com>
2023-05-24 07:00:29 -0700

    second print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of code
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
