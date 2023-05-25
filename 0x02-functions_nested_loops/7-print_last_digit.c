718d55fec89722b397905d2be56ac2fcf8768b3d
Akamo Philip <pakamo20@gmail.com>
2023-05-25 16:56:04 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,10 +6,13 @@
  */
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
-		n *= -1;
+		n = -n;
 
-	_putchar('0' + (n % 10));
+	last_digit = n % 10;
+	_putchar(last_digit + '0');
 
-	return (0 % 10);
+	return (last_digit);
 }
