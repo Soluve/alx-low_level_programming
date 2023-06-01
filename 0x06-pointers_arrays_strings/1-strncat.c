ec4a4af7ebbd2f593cd7139088f4ff35cd20c25c
Akamo Philip <pakamo20@gmail.com>
2023-06-01 06:29:22 -0700

    1-strcat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,20 +8,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
+	int i, j;
 
-	while (*dest)
-		dest++;
-	for (i = 0; i < n; i++)
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
 	{
-		while (*src)
-		{
-			*dest = *src;
-			dest++;
-			src++;
-		}
-	*dest = '\0';
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+
+	dest[i] = '\0';
 
 	return (dest);
 }
