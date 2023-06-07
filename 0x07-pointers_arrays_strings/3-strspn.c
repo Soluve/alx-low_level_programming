359e3c65d5b4fd9f4ff42571a8f2caeffa8470f2
Akamo Philip <pakamo20@gmail.com>
2023-06-06 17:44:53 -0700

    initial commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * int _strspn - gets the length of a prefix substring.
+ * @s: string to search
+ * @accept: target matches
+ * Return: number of bytes consecutively matched
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j;
+	int matches = 0;
+
+	while (*(s + i))
+	{
+		for (j = 0; *(accept + j); j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				matches++;
+				break;
+			}
+			if (*(accept + j + 1) == '\0' && *(s + i) != *(accept + j))
+				return (matches);
+		}
+		i++;
+	}
+	return (matches);
+}
