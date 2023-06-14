b42b3fbbebbda2362d0de7f0d0823f5234bd2fc3
Akamo Philip <pakamo20@gmail.com>
2023-06-14 14:25:00 -0700

    what is my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,10 @@
  * @argv: array of arguments
  * Return: 0
  */
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", char *argv[]);
+	printf("%s\n", *argv);
+
 	return (0);
 }
+
