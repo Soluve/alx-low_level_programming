307f8a7734dcaeb920f1fa8e5595b9ae871d630a
Akamo Philip <pakamo20@gmail.com>
2023-06-14 13:04:20 -0700

    what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print name of program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: EXIT_SUCCESS
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (EXIT_SUCCESS);
+}
